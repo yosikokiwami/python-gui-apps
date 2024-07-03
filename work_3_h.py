5cf64812080b6f071889cdf4c73fa4d2043a9271
yosikokiwami <yamato.dowango@gmail.com>
2024-07-03 12:35:48 +0900

    ワーク３

--- work_3_h.py
+++ work_3_h.py
@@ -7,7 +7,16 @@ window.geometry("600x400")
 bg_color = "#333333"  # ダークグレー
 fg_color = "#FFFFFF"  # 白
 window.configure(bg=bg_color)
+
+
 # ↑↑↑ お約束のコード ↑↑↑
+def add_button_action():  # 関数の定義 ※ボタンが押されたときの動き
+    name = entry1.get()  # 入力値を取得
+    # リストにたす
+    name_list.append(name)
+    hyouji_moji = "\n".join(name_list)  # 改行を挟む
+    label7.config(text=f"{hyouji_moji}")
+
 
 """
 データ型
@@ -28,13 +37,17 @@ name_list = []
 # Pythonで改行する文字は→”\n” (“エミリー\nマック\nジョン\n”)
 
 # TODO 1. ラベル: 名前を入力してください
-
+label1 = tk.Label(window, text="名前を入力して下さい", bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
 # TODO 2. エントリー: 名前を入力するための入力フィールド
-
+entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
+entry1.pack(pady=10)
 # TODO 3. ボタン: ボタンを押すと配列に名前が追加される
-
+button1 = tk.Button(window, text="追加", command=add_button_action)  # 足し算ボタン
+button1.pack(pady=10)
 # TODO 4. ラベル: 配列に追加された名前を表示する
-
+label7 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label7.pack(pady=10)
 # ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
 # ↑↑↑ お約束のコード ↑↑↑
